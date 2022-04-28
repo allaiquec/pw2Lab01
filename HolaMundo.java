b2b7701ab5024374a7eaa158435416ac5c608aa6
Angie Llaique Chullunquia <allaiquec@unsa.edu.pe>
2022-04-28 07:42:32 -0500

    Agregué mi nombre

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,7 @@ public class HolaMundo{
 	public static void main(String args[]){	
 		System.out.println("HolaMundo");
 		System.out.println("Bienvenidos al curso de Pweb2");
+		System.out.println("Angie Llaique Chullunquia");
 	}
 
 }
