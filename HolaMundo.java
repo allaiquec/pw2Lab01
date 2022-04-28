d20e4495f5bb37f4c106e3d70cf447033e6bc9f4
Angie Llaique Chullunquia <allaiquec@unsa.edu.pe>
2022-04-28 07:40:20 -0500

    Agregando bienvenidos al curso de Pweb2

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,7 +1,7 @@
 public class HolaMundo{
-	public static void main(String args[]){
-	
+	public static void main(String args[]){	
 		System.out.println("HolaMundo");
+		System.out.println("Bienvenidos al curso de Pweb2");
 	}
 
 }
